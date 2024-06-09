5c71b95448a54cd94d56f69358760205a6ad2f12
SakshyamStha <sakshamstha6785@gmail.com>
2024-06-09 14:41:25 +0545

    implemented session in register.php

--- register.php
+++ register.php
@@ -1,3 +1,71 @@
+<?php
+
+    session_start();
+
+    include('server/connection.php');
+   
+    if(isset($_POST['register'])){
+      
+     $name =  $_POST['name'];
+     $email =  $_POST['email'];
+     $password =  $_POST['password'];
+     $confirmPassword =  $_POST['confirmPassword'];
+
+
+    if($password !== $confirmPassword){
+      header('location: register.php?error=passwords donot match.');
+     }
+
+    else if(strlen($password) < 8)
+     {
+      header('location: register.php?error= password should atleast be of 8 characters.');
+     }
+     
+
+     
+    else{
+      //to check if email is available
+      $stmt1 = $conn->prepare("SELECT count(*) FROM users WHERE user_email=?");
+
+      $stmt1->bind_param('s',$email);
+      $stmt1->execute();
+      $stmt1->bind_result($num_rows);
+      $stmt1->store_result();
+      $stmt1->fetch();
+ 
+      //if email is already there
+      if($num_rows != 0){
+       header('location: register.php?error=email already taken!');
+      }
+      else{
+        //create a new user
+          $stmt = $conn->prepare("INSERT INTO users (user_name,user_email,user_password)
+          VALUES (?,?,?)");
+
+          $stmt->bind_param('sss',$name,$email,md5($password));
+
+          if($stmt->execute()){
+            $_SESSION['user_email'] = $email;
+            $_SESSION['user_name'] = $name;
+            $_SESSION['logged_in'] = true;
+            header('location: account.php?register=You registered successfully');
+          }
+          else{
+            header('location: register.php?error=couldnot create an accoint at the moment.');
+          }
+      }
+ 
+ 
+      
+ 
+ 
+    }
+    }else if(isset($_SESSION['logged_in'])){
+      header('location: account.php');
+      exit;
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -61,7 +129,8 @@
         </div>
 
         <div class="mx-auto container">
-            <form id="register-form" action="">
+            <form id="register-form" method="POST" action="register.php">
+              <p style = "color:red;"><?php if(isset($_GET['error'])){ echo $_GET['error'];} ?></p>
                 <div class="form-group">
                     <label for="">Name</label>
                     <input type="text" class="form-control" id="register-name" name="name" placeholder="Username">
@@ -80,10 +149,10 @@
                 </div>
 
                 <div class="form-group">
-                   <input type="submit" class="btn" id="register-btn" value="Register">
+                   <input type="submit" class="btn" id="register-btn" name="register" value="Register">
                 </div>
                 <div class="form-group">
-                    <a id="login-url" class="btn" href="">Do you have an Account? Login</a>
+                    <a id="login-url" href="login.php" class="btn" href="">Do you have an Account? Login</a>
                 </div>
             </form>
         </div>
