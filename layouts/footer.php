afafb9eb3f8e0f21c28f0320344c2ef67c3151e7
SakshyamStha <sakshamstha6785@gmail.com>
2024-06-10 22:24:33 +0545

    implemented layout on all pages

---
+++ layouts/footer.php
@@ -0,0 +1,68 @@
+<!--footer-->
+<footer class="mt-5 py-5">
+        <div class="row container mx-auto pt-5">
+          <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+            <img src="assets/imgs/logo1.jpg" height="60px" width="100px" alt="">
+            <p class="pt-3">
+              We provide the best products for affordable prices
+            </p>
+          </div>
+
+          <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+           <h5 class="pb-2">
+            Featured
+           </h5>
+           <ul class="text-uppercase">
+              <li><a href="">Sofa</a></li>
+              <li><a href="">Table</a></li>
+              <li><a href="">Carpet</a></li>
+              <li><a href="">Wall</a></li>
+              <li><a href="">Lamp</a></li>
+              <li><a href="">Chair</a></li>
+           </ul>
+          </div>
+
+          <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+            <h5 class="pb-2">
+              Contact Us
+            </h5>
+            <div>
+              <h6 class="text-uppercase">Address</h6>
+              <p>Samakhushi,Kathmandu</p>
+            </div>
+            <div>
+              <h6 class="text-uppercase">Phone</h6>
+              <p>+977 9841491234</p>
+            </div>
+            <div>
+              <h6 class="text-uppercase">Email</h6>
+              <p>halo1@email.com</p>
+            </div>
+          </div>
+          
+          <div class="footer-two col-lg-3 col-md-6 col-sm-12">
+            <div class="row container mx-auto">
+              <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
+              <a href="#"><i class="fab fa-facebook"></i></a><br>
+              <a href="#"><i class="fab fa-instagram"></i></a><br>
+              <a href="#"><i class="fab fa-twitter"></i></a><br>
+              </div>
+            </div>
+          </div>  
+
+        </div>
+
+        <div class="copyright mt-5">
+          <div class="row container mx-auto">
+            <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
+              <img src="assets/imgs/payment.png" height="77%" width="88%"  alt="">
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12 mb-4 text-nowrap mb-2">
+              <p>Halo @ 2024 All Rights Reserved</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
