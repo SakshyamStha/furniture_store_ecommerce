126600acbcd694d71c71ba2c3d408f25ee760c4f
SakshyamStha <sakshamstha6785@gmail.com>
2024-06-11 17:45:59 +0545

    optimized site

--- product.php
+++ product.php
@@ -32,56 +32,12 @@ if(isset($_GET['product_id'])){
 
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+<?php
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-   
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+include('layouts/header.php');
 
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
-        <div class="container">
-          <img src="assets/imgs/logo1.jpg" height="50" width="100" alt="Logo">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse nav-btns" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              
-              <li class="nav-item">
-                <a class="nav-link" href="index.php">Home</a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="shop.php">Shop</a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="index.php#Categories">Category</a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="contact.php">Contact Us</a>
-              </li>
-
-              <li class="nav-item">
-                <a href="cart.php"><i class="fa-solid fa-cart-shopping"></i></a>
-                <a href="account.php"><i class="fa-solid fa-user"></i></a>
-              </li>
-              
-              
-
-          </div>
-        </div>
-</nav>
+?>
 
       <section class="single-product my-5 pt-5">
         <div class="row mt-5">
--- shop.php
+++ shop.php
@@ -47,76 +47,14 @@ if(isset($_POST['search'])){
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shop</title>
+<?php
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+include('layouts/header.php');
+
+
+?>
+
    
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <link rel="stylesheet" href="assets/css/style.css">
-
-
-    <style>
-        .product img{
-            width: 100%;
-            height: auto;
-            box-sizing: border-box;
-            object-fit: cover;
-        }
-
-        .pagination a{
-            color: coral;
-        }
-
-        .pagination li:hover a{
-            color: white;
-            background-color: coral;
-        }
-    </style>
-</head>
-<body>
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
-        <div class="container">
-          <img src="assets/imgs/logo1.jpg" height="50" width="100" alt="Logo">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse nav-btns" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              
-              <li class="nav-item">
-                <a class="nav-link" href="index.php">Home</a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="shop.php">Shop</a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="index.php#Categories">Category</a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="contact.php">Contact Us</a>
-              </li>
-
-              <li class="nav-item">
-                <a href="cart.php"><i class="fa-solid fa-cart-shopping"></i></a>
-                <a href="account.php"><i class="fa-solid fa-user"></i></a>
-              </li>
-              
-              
-
-          </div>
-        </div>
-    </nav>
-
 
       <section id="search" class="my-5 py-5 ms-2 ">
         <div class="container mt-5 py-2">
