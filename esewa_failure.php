0b6818503d98679b0e436ac39a7afdbca5672de9
SakshyamStha <sakshamstha6785@gmail.com>
2024-06-14 17:09:45 +0545

    added esewa_failure page

---
+++ esewa_failure.php
@@ -0,0 +1,13 @@
+<?php
+
+include('layouts/header.php');
+
+echo '<section class="my-5 py-5">
+                    <div class="container text-center mt-3 pt-5">
+                        <h2>Server connection error! Please try again later!!!</h2>';
+                
+echo '</div></section>';
+
+
+include('layouts/footer.php');
+?>
