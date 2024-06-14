629d2bef26045c3eaa474fb7f823f17fc4b064d3
mimi428 <mihika.ranjit1@gmail.com>
2024-06-14 21:18:46 +0545

    shopping icon

--- layouts/header.php
+++ layouts/header.php
@@ -15,7 +15,7 @@ session_start();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+    
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
@@ -47,7 +47,7 @@ session_start();
 
               <li class="nav-item">
                 <a href="cart.php">
-                  <i class="fa-solid fa-cart-shopping">
+                <i class="fa-brands fa-shopify">
                     <?php if(isset($_SESSION['quantity']) && $_SESSION['quantity'] != 0) { ?>
 
                       <span class= "cart-quantity"><?php echo $_SESSION['quantity']; ?></span>
