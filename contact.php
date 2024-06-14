111bfff69e1de88baf35b37604e326f6d7345155
mimi428 <mihika.ranjit1@gmail.com>
2024-06-14 21:50:39 +0545

    map added

--- contact.php
+++ contact.php
@@ -1,12 +1,6 @@
 <?php
-
 include('layouts/header.php');
-
-
 ?>
-
-
-
       <!---Contact---->
       <section id="contact" class="container my-5 py-5"> 
         <div class="container text-center nt-5">
@@ -14,17 +8,17 @@ include('layouts/header.php');
         <br>
         <h3>Contact us</h3>
         <hr class="nx-auto">
+        <br>
         <p class="w-50 mx-auto">Phone number:<span>9813919990</span></p>
-        <p class="w-50 mx-auto"> Email address: <span>mihika@gmail.com</span></p>
+        <p class="w-50 mx-auto"> Email address: <span>halo@gmail.com</span></p>
         <p class="-50 mx-auto">We work 24/7 to answer your questions
         </p> 
         </div>
-      </section>
       
-    
-      
-
-   
+      <center><div class="map-div"> 
+      <div style="width: 100%"><iframe width="50%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=50%25&amp;height=300&amp;hl=en&amp;q=Nccs%20college%20+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps systems</a></iframe></div>
+        </div> </center>
+        </section>
 <?php
   include('layouts/footer.php');
 ?>
